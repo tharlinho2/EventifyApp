6c70e245e655622ac456c47cecca97b2ddc5dc37
Tharles Ribeiro Pereira <tharlinho2@gmail.com>
2022-04-26 14:37:50 -0300

    configuration Sidekiq

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,14 @@
 module EventsHelper
+  def calculateDate(value, time_unit, starts_at)
+    case time_unit
+    when "MINUTE"
+      return starts_at - value.minutes
+    when "HOUR"
+      return starts_at - value.hours
+    when "DAY"
+      return starts_at - value.day
+    when "WEEK"
+      return starts_at - value.week
+    end
+  end
 end
