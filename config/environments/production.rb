9c7b5fac7f34a3e9d72fd8ea52874cf556401357
Tharles Ribeiro Pereira <tharlinho2@gmail.com>
2022-05-02 18:30:43 -0300

    Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: ENV["MAILGUN_API_KEY"],
-    domain: "eventify-th-app.heroku.com",
+    domain: ENV["MAILGUN_DOMAIN"],
   # api_host: 'api.eu.mailgun.net'  # Uncomment this line for EU region domains
   }
 
