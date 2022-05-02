8d1c64206e76817b2df16f55e1786ab4894970bf
Tharles Ribeiro Pereira <tharlinho2@gmail.com>
2022-05-02 17:41:50 -0300

    sidekiq2

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
   resources :events
   resources :reminders, only: %i[ create destroy ]
 
-  authenticate :user, ->(user) { user.admin? } do
-    mount Sidekiq::Web => "/sidekiq"
+  def draw(routes_name)
+    instance_eval(File.read(Rails.root.join("config/routes/#{routes_name}.rb")))
   end
+
+  draw :sidekiq
 end
