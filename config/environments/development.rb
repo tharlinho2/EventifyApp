9bfa56ad6efb8f2bdb247487f63341633fbc03fb
Tharles Ribeiro Pereira <tharlinho2@gmail.com>
2022-05-03 11:27:06 -0300

    email development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,16 +38,9 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: "mailcatcher", port: 1025 }
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-    api_key: ENV["MAILGUN_API_KEY"],
-    domain: ENV["MAILGUN_DOMAIN"],
-  # api_host: 'api.eu.mailgun.net'  # Uncomment this line for EU region domains
-  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
