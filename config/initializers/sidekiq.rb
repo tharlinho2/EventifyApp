316271f6cb2a65f43529e3973f7850d28da392bf
Tharles Ribeiro Pereira <tharlinho2@gmail.com>
2022-05-03 09:07:20 -0300

    Mailgun

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,7 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV["REDIS_URL"] }
 end
+
+Mailgun.configure do |config|
+  config.api_key = "6c306dc281217674d4277d19164b74f8-fe066263-4181c85f"
+end
