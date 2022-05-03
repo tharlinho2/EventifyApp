3b92ff803f7e8dfbf56e5401294457c9f5cbc0ab
Tharles Ribeiro Pereira <tharlinho2@gmail.com>
2022-05-03 09:20:46 -0300

    Mailgun

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,23 +2,18 @@ class UserMailer < ApplicationMailer
   def welcome(user_id)
     user = User.find(user_id)
     @email = user.email
-    mail(to: @email, subject: "Welcome!").tap do |message|
-      message.mailgun_options = {
-        "tag" => ["abtest-option-a", "beta-user"],
-        "tracking-opens" => true,
-        "tracking-clicks" => "htmlonly",
-      }
-    end
+    mail(to: @email, subject: "Bem vindo!")
   end
 
   def reminder(reminder)
     @user = User.find(reminder.user_id)
     @event = Event.find(reminder.event_id)
+
     mail(to: @user.email, subject: "Lembrete")
   end
 
   def welcome_email
-    mail(to: "tharlinho2@gmail.com", subject: "Welcome!").tap do |message|
+    mail(to: params[:to], subject: "Welcome!").tap do |message|
       message.mailgun_options = {
         "tag" => ["abtest-option-a", "beta-user"],
         "tracking-opens" => true,
